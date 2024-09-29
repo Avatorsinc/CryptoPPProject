638bd3b2b796a64c3e7243b1bc256e0d07b83897
Arkadiusz Sikora <avatorsinc@gmail.com>
2024-09-29 13:29:47 +0200

    Add project files.

---
+++ CryptoPPProject.cpp
@@ -0,0 +1,12 @@
+﻿// CryptoPPProject.cpp : Defines the entry point for the application.
+//
+
+#include "CryptoPPProject.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
